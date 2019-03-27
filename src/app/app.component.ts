6362711d186f70e084ee884c6b095d4847475b84
luk9like <lukas.mueller96@gmail.com>
2019-03-27 21:56:53 +0100

    Off Canvas Menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'gc-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'gc-app';
+
+  /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+  openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+    document.getElementById("main").style.marginRight = "100%";
+  }
+
+  /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+  closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginRight = "0";
+  }
+
+  ngOnInit() {
+  }
 }
